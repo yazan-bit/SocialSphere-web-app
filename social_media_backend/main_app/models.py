4fa24ae72364b70d58312109f73661aeb9c0439b
yazan <yazanyousef661@gmail.com>
2025-08-20 04:05:22 +0300

    add custom user model

--- social_media_backend/main_app/models.py
+++ social_media_backend/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True,null=False)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    def __str__(self):
+        return self.username
+    
--- social_media_backend/social_media_backend/settings.py
+++ social_media_backend/social_media_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Default Auth User
+AUTH_USER_MODEL = 'users.User'
