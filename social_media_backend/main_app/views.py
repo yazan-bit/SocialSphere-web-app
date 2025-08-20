19e1387c1a0602f6941422bdd3d9b8f9b7dd8c5f
yazan <yazanyousef661@gmail.com>
2025-08-20 04:57:27 +0300

    adding urls.py

--- social_media_backend/main_app/views.py
+++ social_media_backend/main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework.views import APIView
 # Create your views here.
+
+class PostListView(APIView):
+    def get(self,request):
+        return Response({
+                'posts':['post1','post2']
+            })
